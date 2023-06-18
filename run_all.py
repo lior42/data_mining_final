98fee4054d2486d2d390a81995cb62865c2a200b
lior42 <liorlander@gmail.com>
2023-06-18 10:07:53 +0300

    Changed output to file

--- run_all.py
+++ run_all.py
@@ -4,6 +4,7 @@ from subprocess import run, PIPE
 IMAGES_LIB = Path.cwd() / "testing_images"
 PROG_LOC = Path.cwd() / "src" / "main.py"
 PY_PATH = Path.cwd() / ".venv" / "Scripts" / "python.exe"
+OUT_FILE = Path.cwd() / "out.txt"
 
 all_images = IMAGES_LIB.glob("**/*")
 
@@ -12,7 +13,9 @@ res = ""
 for img in all_images:
     if img.is_dir(): continue
     output = run([str(PY_PATH), str(PROG_LOC), "-i", str(img)], stdout=PIPE)
-    res += str(img.relative_to(Path.cwd()))
-    res += output.stdout.decode() + ("-" * 50) + "\n"
+    x = str(img.relative_to(Path.cwd()))
+    x += output.stdout.decode() + ("-" * 50) + "\n"
+    res += x
+    print(x)
 
-print(res)
+OUT_FILE.write_text(res)
